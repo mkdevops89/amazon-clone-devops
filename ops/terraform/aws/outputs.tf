1039e1963a9592771ce50448b48584642d468301
blisboa <mlis682@gmail.com>
2026-01-07 16:34:29 -0600

    Infra: Add outputs.tf to export resource endpoints

---
+++ ops/terraform/aws/outputs.tf
@@ -0,0 +1,34 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "eks_cluster_endpoint" {
+  description = "Endpoint for EKS Control Plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "rds_endpoint" {
+  description = "RDS MySQL Endpoint"
+  value       = module.db.db_instance_endpoint
+}
+
+output "redis_endpoint" {
+  description = "Redis Cache Endpoint"
+  value       = module.elasticache.replication_group_primary_endpoint_address
+}
+
+output "mq_broker_id" {
+  description = "Amazon MQ Broker ID"
+  value       = aws_mq_broker.rabbitmq.id
+}
+
+output "mq_console_url" {
+  description = "Amazon MQ Web Console URL"
+  value       = aws_mq_broker.rabbitmq.instances[0].console_url
+}
