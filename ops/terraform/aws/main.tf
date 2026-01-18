183b370b15e0485714f0240204ab2470ca9f0956
blisboa <mlis682@gmail.com>
2026-01-17 22:19:30 -0600

    Update EKS Cluster version to 1.33

--- ops/terraform/aws/main.tf
+++ ops/terraform/aws/main.tf
@@ -38,7 +38,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "~> 20.0"
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.33"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
   
